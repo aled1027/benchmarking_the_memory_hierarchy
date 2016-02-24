69e0a67c7ced67db17f9cdf91672778ff374c3f1
aled1027 <a.led1027@gmail.com>
2016-02-23 22:35:51 -0800

    new data

--- c_code/run.sh
+++ c_code/run.sh
@@ -12,10 +12,12 @@ ITERS=1000000
 #    done
 #done
 
-for (( ARR_SIZE = 2**10; ARR_SIZE <= 2**24; ARR_SIZE *= 2))
+for (( ARR_SIZE = 2**10; ARR_SIZE <= 2**24; ARR_SIZE *= 4))
 do
+    echo $ARR_SIZE
+    sleep 0.2
     echo "arr_size, iters, time" > ../data/$ARR_SIZE-$ITERS.csv
-    for (( k = 0; k < 500; k++))
+    for (( k = 0; k < 100; k++))
     do
         ./a.out $ARR_SIZE $ITERS >> ../data/$ARR_SIZE-$ITERS.csv
     done
