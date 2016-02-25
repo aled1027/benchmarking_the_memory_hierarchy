c305ca86f6ca09a02d6bc6295769c48b79e281ca
aled1027 <a.led1027@gmail.com>
2016-02-25 09:29:18 -0800

    checking new data

--- c_code/run.sh
+++ c_code/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ITERS=1000000
-
+#ITERS=1000000
 #for (( ARR_SIZE = 2**10; ARR_SIZE <= 2**24; ARR_SIZE *= 2))
 #do
 #    echo $ARR_SIZE
@@ -12,15 +11,13 @@ ITERS=1000000
 #    done
 #done
 
-for (( ARR_SIZE = 2**10; ARR_SIZE <= 2**24; ARR_SIZE *= 4))
+ARR_SIZE=4096
+ITERS=1000000
+
+echo "arr_size, iters, time" > ../new_data/$ARR_SIZE-$ITERS.csv
+for (( k = 0; k < 100; k++))
 do
-    echo $ARR_SIZE
-    sleep 0.2
-    echo "arr_size, iters, time" > ../data/$ARR_SIZE-$ITERS.csv
-    for (( k = 0; k < 100; k++))
-    do
-        ./a.out $ARR_SIZE $ITERS >> ../data/$ARR_SIZE-$ITERS.csv
-    done
+    ./a.out $ARR_SIZE $ITERS >> ../new_data/$ARR_SIZE-$ITERS.csv
 done
 
 
